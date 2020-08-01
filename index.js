f3a5fc873f58f408586d343d5e19e178e67890a4
Luyny <l.f3rn4ndes@gmail.com>
2020-07-31 21:58:18 -0300

    added changes for heroku

--- index.js
+++ index.js
@@ -1,18 +1,79 @@
 const Discord = require('discord.js')
+const fs = require('fs');
+
 const bot = new Discord.Client()
 // https://discordapp.com/oauth2/authorize?client_id=738804140112871424&scope=bot&permissions=8
 
 const token = 'NzM4ODA0MTQwMTEyODcxNDI0.XyRO-Q.vpbAHUh91PMU7YEPxCCrUfO7P20'
+
+let rawdata = fs.readFileSync('players.json');
+let players = JSON.parse(rawdata);
 
 bot.on('ready', ()=>{
     console.log('Bot ONLINE!')
 })
 
 bot.on('message', msg =>{
-    if (msg.content === 'Hello'){
-        msg.reply("Hello Friend!")
+    var message = msg.content.split(" ")
+    var command = message[0]
+    
+    if (command === "!listall"){
+        players.list.forEach((player)=>{
+            msg.channel.send(`${player.name}: ${player.points}`)
+        })
+    }
+    
+    if (command === "twb!rank"){
+        var user = message[1]
+        players.list.forEach((player)=>{
+            if (player.name == user) {
+                msg.channel.send(`Pontuação de ${player.name}: ${player.points}`)
+            }
+        })
+    }
+
+    if (command === "!addPoints"){
+        var user = message[1]
+        var points = message[2]
+        players.list.forEach((player)=>{
+            if (player.name == user) {
+                player.points += parseInt(points)
+                msg.channel.send(`${player.name} agora tem: ${player.points} pontos`)
+            }
+        })
+        savePlayers()
+    }
+
+
+    if (command === "!add"){
+
+        message.shift()
+        message.forEach((name)=>{
+            players.list.push({name: name,
+                          points: 0})
+        })
+            msg.reply("Adicionados com sucesso")
+            savePlayers()
+    }
+
+    if (command === "!setPoints"){
+        var user = message[1]
+        var points = message[2]
+        players.list.forEach((player)=>{
+            if (player.name == user) {
+                player.points = parseInt(points)
+                msg.channel.send(`${player.name} agora tem: ${player.points} pontos`)
+            }
+        })
+        savePlayers()
     }
 });
 
 bot.login(token)
+
+function savePlayers(){    
+    let data = JSON.stringify(players);
+    fs.writeFileSync('players.json', data);
+}
+
 
