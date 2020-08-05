8d64d6ec5af78294090ddd4fc39afd423664b823
Luyny <l.f3rn4ndes@gmail.com>
2020-08-04 23:36:15 -0300

    add "points" command to help+

--- index.js
+++ index.js
@@ -314,10 +314,11 @@ bot.on('message', async msg => {
             msg.channel.send(
                 "**`twb!rank`** = Lista o Top 10 do rank." +
                 "\n**`twb!say [mensagem]`** = Faz o bot falar algo." +
-                "\n**`twb!remove [player]`** = Deleta um player da lista (Não pode ser revertido, use com cautela)" +
-                "\n**`twb!winner [@player]`** = Anuncia quem foi o ganhador do torneio" +
-                "\n**`twb!add [player] [vitorias] [torneios]`** = Adiciona um player à lista (se já existir, irá adicionar os dados informados ao player)" +
-                "\n**`twb!set [data] [horario]`**= Altera a data do torneio (Horario é opcional). Coloque [data] como 0 para cancelar o torneio.")
+                "\n**`twb!remove [player]`** = Deleta um player da lista (Não pode ser revertido, use com cautela)." +
+                "\n**`twb!winner [@player]`** = Anuncia quem foi o ganhador do torneio." +
+                "\n**`twb!add [player] [vitorias] [torneios]`** = Adiciona um player à lista (se já existir, irá adicionar os dados informados ao player)." +
+                "\n**`twb!set [data] [horario]`**= Altera a data do torneio (Horario é opcional). Coloque [data] como 0 para cancelar o torneio." +
+                "\n**`twb!points [nome] [pontos]`** = Altera os pontos de um jogador. Use um numero positivo para adicionar, ou um numero negativo para subtrair (Ex: -100).")
         },
 
         "points"() {
