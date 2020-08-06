a5df491cd58b4ad8bdfcebdb7becae36f1262ad8
Luyny <l.f3rn4ndes@gmail.com>
2020-08-06 20:57:02 -0300

    change tournament message

--- commands/tournament.js
+++ commands/tournament.js
@@ -14,7 +14,7 @@ module.exports = {
         } else {
             msg.channel.send(new Discord.MessageEmbed()
                 .setDescription(
-                    `O próximo torneio vai ocorrer dia **${config.tournament.date}** às **${config.tournament.hour}h**\n\n\n *ESTEJA PRONTO*`)
+                    `O próximo torneio ocorrerá dia **${config.tournament.date}** às **${config.tournament.hour}h**.\nCompareça 20 minutos antes no canal **TORNEIO**, tem até 10 minutos de tolerância ao atraso.\n***BOA SORTE!***`)
                 .setColor('#0099ff')
                 .setTitle('TWB | Torneio')
                 .setAuthor('Ukyo', 'https://cdn.discordapp.com/avatars/420610175590989825/ec14531ea3f9767a7f8e39db8d4b73a8.png'))
