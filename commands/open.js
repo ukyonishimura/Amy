b56f7f93f9904f644dd12e650d81e72596b17cb9
Luyny <l.f3rn4ndes@gmail.com>
2020-08-07 22:58:14 -0300

    change open command to set entry day

--- commands/open.js
+++ commands/open.js
@@ -1,9 +1,9 @@
 module.exports = {
     name: 'open',
-    description: "**`twb!open`** = Informa que as inscrições para o torneio já estao abertas!",
+    description: "**`twb!open [data]`** = Altera a data das inscrições para o torneio!",
     needsPermission: true,
     execute(msg, args) {
-        const { config, allowedRole } = require('../index.js');
+        const { config, allowedRole ,saveConfig} = require('../index.js');
         const Discord = require('discord.js')
 
         if (!msg.member.roles.cache.some(role => role.name === allowedRole)) {
@@ -11,12 +11,13 @@ module.exports = {
         }
         if (args.length == 0 ) return
         
+        config.tournament.entry = args
         msg.channel.send(new Discord.MessageEmbed()
             .setColor("#fcf403")
             .setAuthor(`TORNEIO | TWB`)
-            .setDescription(`**As inscrições para o torneio já estão abertas!!\nInscrições abertas até o dia ${args}.**`)
+            .setDescription(`**As inscrições para o torneio estarão abertas até o dia ${config.tournament.entry}.**`)
         )
-
+        saveConfig()
         msg.delete()
     }
 }
--- commands/tournament.js
+++ commands/tournament.js
@@ -5,7 +5,7 @@ module.exports = {
     execute(msg, args) {
         const { config } = require('../index.js');
         const Discord = require('discord.js')
-
+        var entry = ""
         if (config.tournament.date == "0") {
             msg.channel.send(new Discord.MessageEmbed()
                 .setDescription("Não há nenhum torneio programado no momento.\nFique atento para mais informações.")
@@ -13,9 +13,12 @@ module.exports = {
                 .setTitle('TWB | Torneio')
                 .setAuthor('Ukyo', 'https://cdn.discordapp.com/avatars/420610175590989825/ec14531ea3f9767a7f8e39db8d4b73a8.png'))
         } else {
+            if (config.tournament.entry != "0") {
+                entry = `\nInscrições abertas até o dia **${config.tournament.entry}**.`
+            }
             msg.channel.send(new Discord.MessageEmbed()
                 .setDescription(
-                    `O próximo torneio ocorrerá dia **${config.tournament.date}** às **${config.tournament.hour}h**.\nCompareça 20 minutos antes no canal **TORNEIO**, tem até 10 minutos de tolerância ao atraso.\n***BOA SORTE!***`)
+                    `O próximo torneio ocorrerá dia **${config.tournament.date}** às **${config.tournament.hour}h**.${entry}\nCompareça 20 minutos antes no canal **#TORNEIO**, tem até 10 minutos de tolerância ao atraso.\n***BOA SORTE!***`)
                 .setColor('#0099ff')
                 .setTitle('TWB | Torneio')
                 .setAuthor('Ukyo', 'https://cdn.discordapp.com/avatars/420610175590989825/ec14531ea3f9767a7f8e39db8d4b73a8.png'))
