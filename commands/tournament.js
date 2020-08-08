ab8c49e99d724d44637a233a26996fb6d731e5ed
ukyonishimura <ukyonishimura@gmail.com>
2020-08-08 02:13:06 +0000

    torneio

--- commands/tournament.js
+++ commands/tournament.js
@@ -18,7 +18,7 @@ module.exports = {
             }
             msg.channel.send(new Discord.MessageEmbed()
                 .setDescription(
-                    `O próximo torneio ocorrerá dia **${config.tournament.date}** às **${config.tournament.hour}h**.${entry}\nCompareça 20 minutos antes no canal **#TORNEIO**, tem até 10 minutos de tolerância ao atraso.\n***BOA SORTE!***`)
+                    `O próximo torneio ocorrerá dia **${config.tournament.date}** às **${config.tournament.hour}h**.${entry}\nCompareça 20 minutos antes no canal de voz **#Torneio**, tem até 10 minutos de tolerância ao atraso.\n***Boa Sorte!***`)
                 .setColor('#0099ff')
                 .setTitle('TWB | Torneio')
                 .setAuthor('Ukyo', 'https://cdn.discordapp.com/avatars/420610175590989825/ec14531ea3f9767a7f8e39db8d4b73a8.png'))
